5b6fe4649d36d9b445ae0380780d036d1a3f66a7
vikas_dangi <vikasdangi8moon@gmail.com>
2022-11-05 19:10:31 +0530

    implemented code for general case

--- L.5. Matrix Multiplication Using Threads/sampleThreadProgram.c
+++ L.5. Matrix Multiplication Using Threads/sampleThreadProgram.c
@@ -10,10 +10,10 @@ static void multiply_seq();
 bool verify();
 
 
-#define NTHREADS 4
+// #define NTHREADS 4
 #define SIZE 1000
 
-static thread_t threads[NTHREADS];
+// static thread_t threads[NTHREADS];
 
 static float A[SIZE][SIZE];
 static float B[SIZE][SIZE];
@@ -34,12 +34,14 @@ void set_mem()
 	}
 }
 
+int NTHREADS;
 int main(int argc,char** argv){
 	int i;
 	long exitValue;
-
+	printf("Enter the value of n:\n");
+	scanf("%d", &NTHREADS);
 	set_mem();
-
+	thread_t threads[NTHREADS];
 	for(i=0;i<NTHREADS;i++){
 		thread_create(&(threads[i]),&multiply,i);
 	}
@@ -49,21 +51,21 @@ int main(int argc,char** argv){
 		printf("Thread %d returned with %ld\n",i,exitValue);
 	}
 	printf("Main thread done.\n");
-	multiply_seq();
-	if(verify())
-	{
-		printf("Multiplication verified\n");
-	}
-	else 
-	{
-		printf("Verification failed\n");
-	}
+	// multiply_seq();
+	// if(verify())
+	// {
+	// 	printf("Multiplication verified\n");
+	// }
+	// else 
+	// {
+	// 	printf("Verification failed\n");
+	// }
 	return 0;
 }
 
 void multiply(int n){
- 
-    for (int i = n*250; i < (n+1)*250; i++) {
+	int tem=1000/NTHREADS;
+    for (int i = n*tem; i < (n+1)*tem; i++) {
         for (int j = 0; j <SIZE; j++) {
             prod[i][j] = 0;
  
